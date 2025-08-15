4eca711c3fd4d587e63f4472f44115c2470e7c16
sameerindrayan <sameer.indrayan07@gmail.com>
2025-08-14 19:02:39 -0700

    now makes csv table

--- vegas_lines_scraper.py
+++ vegas_lines_scraper.py
@@ -6,33 +6,35 @@ teams = [
     "crd", "atl", "rav", "buf", "car", "chi", "cin", "cle", "dal", "den", "det", "gnb","htx", "clt", "jax", "kan", "sdg", "ram", "rai", "mia", "min", "nwe", "nor", 'nyg', 'nyj', "phi", 'pit', 'sea', 'sfo', 'tam', 'oti', 'was'
 ]
 
-start_time = time.time()
+start_time = time.time()    
 
 seasons = range(2015,2025)
 
 veg_df = pd.DataFrame()
 count = 0
+
 for season in seasons:
     for team in teams:
-        url = 'https://www.pro-football-reference.com/teams/'+ team + '/' + str(season) + '_lines.htm'
-        count+=1
+        url = f'https://www.pro-football-reference.com/teams/{team}/{season}_lines.htm'
+        count += 1
         print(f"{url}, {count} teams have been iterated.")
 
-        # get vegas lines (frm 'vegas_lines' table)
-        lines_df = pd.read_html(url, header=0, attrs={'id':'vegas_lines'})[0]
+        lines_df = pd.read_html(url, header=0, attrs={'id': 'vegas_lines'})[0]
 
-        # insert szn and tm cols
-        lines_df.insert(loc=0, column='Season', value = season)
-        lines_df.insert(loc=1, column='Team', value = team.upper())
+        # rename BEFORE concat
+        lines_df = lines_df.rename(columns={'G#': 'Gtm', 'Over/Under': 'Total'})
 
-        # concat team lines datafram to aggregate df (along rows)
+        # insert season and team
+        lines_df.insert(loc=0, column='Season', value=season)
+        lines_df.insert(loc=1, column='Team', value=team.upper())
+
+        # append to big df
         veg_df = pd.concat([veg_df, lines_df], ignore_index=True)
 
-        time.sleep(random.randint(4,5))
+        time.sleep(random.randint(4, 5))
 
-end_time = time.time()
-
-elapsed_time = end_time - start_time
-print(f'Elapsed Time:  {elapsed_time} seconds')
+# filter once after loop
+veg_df = veg_df.query('(Season <= 2020 and Gtm < 17) or (Season >= 2021 and Gtm < 18)')
 
 print(veg_df.info())
+veg_df.to_csv('nfl_vegas_lines_2015-2024.csv', index=False)
